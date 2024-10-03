5aa1e0765249863ca00d56f06741fdc505bc477a
Laura <laura@gmail.com>
2024-10-03 09:32:30 +0200

    ejerc2

--- ejerc2.js
+++ ejerc2.js
@@ -1,21 +1,21 @@
 
 const imagen = document.createElement('img');
 imagen.setAttribute('src', 'img_la.jpg'); 
+document.body.appendChild(imagen);
 
-// Create a heading element
 const heading = document.createElement('h1');
 heading.textContent = "This is a heading";  
-
+document.body.appendChild(heading);
 
 const para1 = document.createElement('p');
 para1.textContent = "This is a paragraph";  
-
+document.body.appendChild(para1);
 
 const para2 = document.createElement('p');
 para2.textContent = "This is another paragraph"; 
-
-
-document.body.appendChild(imagen);
-document.body.appendChild(heading);
-document.body.appendChild(para1);
 document.body.appendChild(para2);
+
+
+
+
+
